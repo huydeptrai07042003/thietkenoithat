d82deb962e5350a02c27f1ec1d0abcce607d5fe4
huydeptrai07042003 <trinhquanghuy0704@gmail.com>
2025-09-13 21:20:07 +0700

    final

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ const productRoute = require("./routes/productRoutes");
 const uploadRoute = require("./routes/uploadRoute");
 const adminRoutes = require("./routes/adminRoutes");
 
-
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -16,9 +14,6 @@ app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-//connect to DB
-connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to home");
@@ -32,7 +27,9 @@ app.use("/api/upload", uploadRoute);
 //Admin
 app.use("/api/admin/users", adminRoutes);
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+//connect to DB
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
